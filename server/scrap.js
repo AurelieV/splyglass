6ea4d2ad2235bd10b495d4296cb878f104c70c55
Aurélie VIOLETTE <orwel.violette@gmail.com>
2018-09-15 12:16:21 +0200

    Few fix

--- server/scrap.js
+++ server/scrap.js
@@ -1,61 +1,78 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+const rp = require('request-promise')
+const cheerio = require('cheerio')
 
-const MongoClient = require('mongodb').MongoClient;
-const configuration = require('./private.json').database;
-const url = `mongodb+srv://${configuration.user}:${configuration.password}@${configuration.url}`;
+const MongoClient = require('mongodb').MongoClient
+const configuration = require('./private.json').database
+const url = `mongodb+srv://${configuration.user}:${configuration.password}@${
+  configuration.url
+}`
 
-const eventId = 98;
+const eventId = 104
 const options = {
   uri: `http://pairings.channelfireball.com/personal/${eventId}`,
   useNewUrlParser: true,
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-};
+  transform: function(body) {
+    return cheerio.load(body)
+  },
+}
 
-const playerRegexp = /^(.+), (.+) \((\d)+\)$/;
+const playerRegexp = /^(.+), (.+) \((\d)+\)$/
 
 async function start() {
   try {
-    const $ = await rp(options);
-    let players = [];
-    $('#playerlist').children('option').each(function() {
-      const info = playerRegexp.exec($(this).text());
-      if (!info) {
-        console.log("Parse error", $(this).text())
-        return;
-      };
-      const [,lastname, firstname] = info;
-      const playerId = $(this).attr('value');
+    const $ = await rp(options)
+    let players = []
+    $('#playerlist')
+      .children('option')
+      .each(function() {
+        const info = playerRegexp.exec($(this).text())
+        if (!info) {
+          console.log('Parse error', $(this).text())
+          return
+        }
+        const [, lastname, firstname] = info
+        const playerId = $(this).attr('value')
 
-      players.push({
-        lastname,
-        firstname,
-        playerId,
-        logs: [
-          { user: 'Auto', time: (new Date()).getTime(), action: 'Imported', data: {firstname, lastname }}
-        ],
+        players.push({
+          lastname,
+          firstname,
+          playerId,
+          logs: [
+            {
+              user: 'Auto',
+              time: new Date().getTime(),
+              action: 'Imported',
+              data: { firstname, lastname },
+            },
+          ],
+        })
       })
-    })
 
-    console.log(`${players.length} scrapped`);
+    console.log(`${players.length} scrapped`)
 
-    const client = await MongoClient.connect(url);
-    const db = client.db('test');
-    const collection = db.collection('players');
-    const results = await Promise.all(players.map(player =>
-      collection.findOneAndUpdate({ firstname: player.firstname, lastname: player.lastname }, {$set: player}, {upsert: true})
-    ));
-    const newPlayers = results.filter(result => result.lastErrorObject.updatedExisting);
-    console.log(newPlayers[0]);
-    console.log(`${newPlayers.length} new players inserted`);
+    console.log(players[0])
 
-    client.close();
-  }
-  catch (err) {
-    console.log("Error", err);
+    const client = await MongoClient.connect(url)
+    const db = client.db('test')
+    const collection = db.collection('players')
+    const results = await Promise.all(
+      players.map((player) =>
+        collection.findOneAndUpdate(
+          { firstname: player.firstname, lastname: player.lastname },
+          { $set: player },
+          { upsert: true }
+        )
+      )
+    )
+    const newPlayers = results.filter(
+      (result) => result.lastErrorObject.updatedExisting
+    )
+    console.log(`${newPlayers.length} new players inserted`)
+
+    client.close()
+  } catch (err) {
+    console.log('Error', err)
   }
 }
 
-start();
+start()
